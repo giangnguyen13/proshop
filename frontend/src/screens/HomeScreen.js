0952b0a35c470c113f8c979424da80ee485ac5e5
Giang Nguyen <giangkawaii1999@gmail.com>
2021-04-25 15:22:50 -0400

    product detail reducer

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,29 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useEffect } from 'react';
 import Product from '../components/Product';
 import { Row, Col } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../actions/productActions';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 
 const HomeScreen = () => {
-    const [products, setProducts] = useState([]);
+    const dispatch = useDispatch();
+
+    const productList = useSelector((state) => state.productList);
+
+    const { loading, error, products } = productList;
+
     useEffect(() => {
-        const fetchProducts = async () => {
-            const res = await axios.get('/api/products');
-            setProducts(res.data);
-        };
-        fetchProducts();
-    }, []);
+        dispatch(listProducts());
+    }, [dispatch]);
+
     return (
         <>
             <h1>Latest Products</h1>
-            <Row>
-                {products.map((product) => {
-                    return (
-                        <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-                            <Product product={product} />
-                        </Col>
-                    );
-                })}
-            </Row>
+            {loading ? (
+                <Loader />
+            ) : error ? (
+                <Message variant='danger' children={error} />
+            ) : (
+                <Row>
+                    {products.map((product) => {
+                        return (
+                            <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+                                <Product product={product} />
+                            </Col>
+                        );
+                    })}
+                </Row>
+            )}
         </>
     );
 };
