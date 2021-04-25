56f48f815168f72ffc3a4086be092594353c822e
Giang Nguyen <giangkawaii1999@gmail.com>
2021-04-25 12:23:49 -0400

    set up api route

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import asyncHandler from 'express-async-handler';
+import Product from '../models/product.model.js';
+
+const router = express.Router();
+
+/**
+ * @description Fetch all products
+ * @route       GET /api/products
+ * @access      Public
+ */
+router.get(
+    '/',
+    asyncHandler(async (req, res) => {
+        const products = await Product.find({});
+        res.json(products);
+    })
+);
+
+/**
+ * @description Fetch specific product by id
+ * @route       GET /api/products/:id
+ * @param       productId
+ * @access      Public
+ */
+router.get(
+    '/:id',
+    asyncHandler(async (req, res) => {
+        const product = await Product.findById(req.params.id);
+        if (product) {
+            res.json(product);
+        } else {
+            res.status(404).json({ message: 'Product not found' });
+        }
+    })
+);
+
+export default router;
