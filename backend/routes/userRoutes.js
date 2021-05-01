30f899b6dbdd75384da2a3f5c22a3ec83df27d9c
Giang Nguyen <giangkawaii1999@gmail.com>
2021-05-01 10:59:00 -0400

    user authorization

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 
-import { authUser } from '../controllers/user.controller.js';
+import { authUser, getUserProfile } from '../controllers/user.controller.js';
+import { protect } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.route('/login').post(authUser);
+router.route('/profile').get(protect, getUserProfile);
 
 export default router;
