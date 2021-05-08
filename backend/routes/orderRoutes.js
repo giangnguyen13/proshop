5c54a500d471e624a1ac18b8b7556a2867c72119
Giang Nguyen <giangkawaii1999@gmail.com>
2021-05-08 14:27:22 -0400

    get order by id BE

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,14 @@
 import express from 'express';
 
-import { addOrderItems } from '../controllers/order.controller.js';
+import {
+    addOrderItems,
+    getOrderById,
+} from '../controllers/order.controller.js';
 import { protect } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.route('/').post(protect, addOrderItems);
+router.route('/:id').get(protect, getOrderById);
 
 export default router;
