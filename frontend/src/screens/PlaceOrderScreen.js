b3455940304c24f2b1db31b2402f527e23a410f2
Giang Nguyen <giangkawaii1999@gmail.com>
2021-05-24 16:16:10 -0400

    fix redirect to wrong url

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -50,7 +50,7 @@ const PlaceOrderScreen = ({ history }) => {
 
     useEffect(() => {
         if (success) {
-            history.push(`order/${order._id}`);
+            history.push(`orders/${order._id}`);
         }
         // eslint-disable-next-line
     }, [history, success]);
